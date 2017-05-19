f5e2ad47ad5d5a4fe586ee5f94adb9cc4e67dfd3
yoshimarimo <yoneda.yoshiaki@m.alhinc.jp>
2017-05-19 16:12:09 +0900

    final

--- src/jp/alhinc/yoneda_yoshiaki/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/yoneda_yoshiaki/calculate_sales/CalculateSales.java
@@ -16,15 +16,12 @@ import java.util.Map.Entry;
 
 public class CalculateSales {
 
-
-
-	//＜処理内容４-１＞支店別集計結果の出力
 	/////////////////   outputSales method   //////////////
-	public static boolean outputSales(HashMap<String,Long> salesTotalMap, HashMap<String,String> inforMap, String filePath){
-		List<Map.Entry<String,Long>> entries = new ArrayList<Map.Entry<String,Long>>(salesTotalMap.entrySet());
-		Collections.sort(entries, new Comparator<Map.Entry<String,Long>>(){
+	public static boolean outputSales(HashMap<String, Long> salesTotalMap, HashMap<String, String> inforMap, String filePath){
+		List<Map.Entry<String, Long>> entries = new ArrayList<Map.Entry<String, Long>>(salesTotalMap.entrySet());
+		Collections.sort(entries, new Comparator<Map.Entry<String, Long>>(){
 			//@Override
-			public int compare(Entry<String,Long> entry1, Entry<String,Long> entry2){
+			public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2){
 				return((Long)entry2.getValue()).compareTo((Long)entry1.getValue());
 			}
 		});
@@ -57,7 +54,7 @@ public class CalculateSales {
 		return true;
 	}
 
-	public static boolean iutputData(String filepath, String eRule, String error, HashMap<String,String> inforMap, HashMap<String,Long> salesTotalMap){
+	public static boolean iutputData(String filepath, String eRule, String error, HashMap<String, String> inforMap, HashMap<String, Long> salesTotalMap){
 		BufferedReader br = null;
 		try{
 			File file = new File(filepath);
@@ -99,17 +96,16 @@ public class CalculateSales {
 	}
 
 	public static void main(String[] args){
-
-		HashMap<String,String> branchLstmap = new HashMap<String,String>();
-		HashMap<String,Long> branchSalesmap = new HashMap<String,Long>();
-		HashMap<String,String> commodityLstmap = new HashMap<String,String>();
-		HashMap<String,Long> commoditySalesmap = new HashMap<String,Long>();
+		HashMap<String, String> branchLstmap = new HashMap<String, String>();
+		HashMap<String, Long> branchSalesmap = new HashMap<String, Long>();
+		HashMap<String, String> commodityLstmap = new HashMap<String, String>();
+		HashMap<String, Long> commoditySalesmap = new HashMap<String, Long>();
+		
 		ArrayList<String> fileNames = new ArrayList<String>();
 		if(args.length != 1){
 			System.out.println("予期せぬエラーが発生しました");
 			return;
 		}
-
 
 		//例外によるエラー表示処理（予期せぬエラーが発生しました。）
 		/*＜処理内容１＞支店定義ファイル読み込み*/
@@ -134,27 +130,24 @@ public class CalculateSales {
 				fileNames.add(fileName);
 			}
 			//「filenames」リストに格納
-
 		}
+		
 		Collections.sort(fileNames);
-		for(int j = 0; j < (fileNames.size()-1); j++){
-			int k = j + 1;
-			int fileNameA = Integer.parseInt(fileNames.get(j).replaceAll("[^0-9]", ""));
-			int fileNameB = Integer.parseInt(fileNames.get(k).replaceAll("[^0-9]", ""));
+		for(int i = 0; i < (fileNames.size()-1); i++){
+			int fileNameA = Integer.parseInt(fileNames.get(i).replaceAll("[^0-9]", ""));
+			int fileNameB = Integer.parseInt(fileNames.get(i + 1).replaceAll("[^0-9]", ""));
 
 			if((fileNameB - fileNameA) != 1){
 				System.out.println("売上ファイル名が連番になっていません");
 				return;
 			}
 		}
-
-		File sales;
-		FileReader fr = null;
+		
 		BufferedReader br = null;
 		try{
 			for(int i = 0; i < (fileNames.size()); i++){
-				sales = new File(args[0]+ File.separator + fileNames.get(i));
-				fr = new FileReader(sales);
+				File sales = new File(args[0]+ File.separator + fileNames.get(i));
+				FileReader fr = new FileReader(sales);
 				br = new BufferedReader(fr);
 				ArrayList<String> saler = new ArrayList<String>();
 				String s;
